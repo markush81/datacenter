5d1f664f586c16067703a0347ad7a6d73374b52c
Markus Helbig <3304672+markush81@users.noreply.github.com>
2023-05-14 14:51:54 +0200

    updates and shellcheck

--- bin/common.sh
+++ bin/common.sh
@@ -1,16 +1,16 @@
+#!/bin/sh
 readonly FOREGROUND_BLUE='\033[0;34m'
 readonly FOREGROUND_GREEN='\033[1;32m'
 readonly FOREGROUND_RED='\033[0;31m'
 readonly FOREGROUND_YELLOW='\033[1;33m'
 readonly ATTRIBUTES_RESET='\033[0m'
 
-failure() { echo -e "[${FOREGROUND_RED}✘${ATTRIBUTES_RESET}] ${1}"; }
-warning() { echo -e "[${FOREGROUND_YELLOW}w${ATTRIBUTES_RESET}] ${1}"; }
-info() { echo -e "[${FOREGROUND_BLUE}i${ATTRIBUTES_RESET}] ${1}"; }
-success() { echo -e "[${FOREGROUND_GREEN=}✔︎${ATTRIBUTES_RESET}] ${1}"; }
+failure() { printf "[${FOREGROUND_RED}✘${ATTRIBUTES_RESET}] %s" "${1}"; }
+warning() { printf "[${FOREGROUND_YELLOW}w${ATTRIBUTES_RESET}] %s" "${1}"; }
+info() { printf "[${FOREGROUND_BLUE}i${ATTRIBUTES_RESET}] %s" "${1}"; }
+success() { printf "[${FOREGROUND_GREEN=}✔︎${ATTRIBUTES_RESET}] %s" "${1}"; }
 
-function ensure_commands() {
-  local cmd
+ensure_commands() {
   for cmd in "${@}"; do
     if ! command -v "${cmd}" >/dev/null; then
       failure "missing command ${cmd}"
